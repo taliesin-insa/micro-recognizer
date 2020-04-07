c688e567fc5473785baf2664dc40dfd05ddf3036
mdarfay <mdarfay@insa-rennes.fr>
2020-04-07 13:00:32 +0200

    iso(api): fix debug

--- src/micro-recognizer/rest-api.go
+++ src/micro-recognizer/rest-api.go
@@ -109,7 +109,8 @@ func getPictures(client *http.Client) ([]Picture, error) {
 
 	// check whether there was an error during request
 	if response.StatusCode != http.StatusOK {
-		log.Printf("[ERROR] Error during GET request to DB: %d, %v", response.StatusCode, response.Body)
+		var body, _ = ioutil.ReadAll(response.Body)
+		log.Printf("[ERROR] Error during GET request to DB: %d, %v", response.StatusCode, body)
 		return nil, errors.New("bad status")
 	}
 
