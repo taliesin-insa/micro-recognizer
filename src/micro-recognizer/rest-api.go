ba568325d645a75b1d27af81bbd44d3dd0cce2fd
mdarfay <mdarfay@insa-rennes.fr>
2020-05-11 12:34:08 +0200

    log(api): Add response message

--- src/micro-recognizer/rest-api.go
+++ src/micro-recognizer/rest-api.go
@@ -258,7 +258,7 @@ func sendImgsToRecognizer(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// we send a response directly, to avoid blocking the caller while we annotate images with the recognizer
 		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(""))
+		w.Write([]byte("[MICRO-RECOGNIZER] Request accepted"))
 
 		client := &http.Client{}
 
